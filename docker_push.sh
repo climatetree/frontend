4d2cbeaea8e2c5fb6181a9ee8a5e7e8721f1ae92
patelvp <patel.visha@husky.neu.edu>
2020-02-04 18:46:26 -0500

    Fix docker login command bug

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker build -t climatetree:front-end .
 docker tag climatetree:front-end patelvp/climatetree:front-end
 docker push patelvp/climatetree
