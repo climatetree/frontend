8a55ed8c614348be0169e2005c63b7544b153eae
Evan <EvanLDouglass@gmail.com>
2020-02-07 13:13:58 -0800

    Move Search to its own component

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,42 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 import OlMap from './mapsComponents/OlMap';
-import { getPlacesByName } from "../services/fetchPlaces";
+import Search from './mapsComponents/Search';
 
 import '../styles/Maps.css';
 
 const Maps = () => {
-  const [searchPlace, setSearchPlaceTerm] = useState("");
-  const [places, setPlaces] = useState([]);
-
-  const whenUserInputPlace = event => {
-    setSearchPlaceTerm(event.target.value);
-  };
-
-  const onSearchPlaceClick = async () => {
-    const response = await getPlacesByName(searchPlace);
-    setPlaces(response);
-    setSearchPlaceTerm("");
-  };
-
   return (
     <div id='MapsPage'>
       <OlMap mapId="map" />
-      {/* Search Box */}
-      <div id="search">
-        <input
-          type="text"
-          style={{ color: "#000" }}
-          placeholder="Enter a place"
-          value={searchPlace}
-          onChange={event => whenUserInputPlace(event)}
-        />
-        <button style={{ color: "#000" }} onClick={onSearchPlaceClick}>
-          Search
-        </button>
-        <br></br>
-        <span style={{ color: "#000" }}>{places.length}</span>
-      </div>
+      <Search />
     </div>
   );
 };
---
+++ src/components/mapsComponents/Search.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import { getPlacesByName } from "../../services/fetchPlaces";
+
+const Search = (props) => {
+    const [searchPlace, setSearchPlaceTerm] = useState("");
+    const [places, setPlaces] = useState([]);
+
+    const whenUserInputPlace = event => {
+        setSearchPlaceTerm(event.target.value);
+    };
+
+    const onSearchPlaceClick = async () => {
+        const response = await getPlacesByName(searchPlace);
+        setPlaces(response);
+        setSearchPlaceTerm("");
+    };
+
+    return (
+        <div id="search">
+            <input
+                type="text"
+                style={{ color: "#000" }}
+                placeholder="Enter a place"
+                value={searchPlace}
+                onChange={event => whenUserInputPlace(event)}
+            />
+            <button style={{ color: "#000" }} onClick={onSearchPlaceClick}>
+                Search
+            </button>
+            <br></br>
+            <span style={{ color: "#000" }}>{places.length}</span>
+        </div>
+    );
+}
+
+export default Search;
