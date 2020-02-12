b7d7217856479a655b524006d569de66a779c8d3
Nick <nick@Nicks-MacBook-Pro-2.local>
2020-02-11 22:30:27 -0800

    filter changes

--- src/components/mapsComponents/SearchBar.js
+++ src/components/mapsComponents/SearchBar.js
@@ -1,33 +1,70 @@
 import React, { useState } from 'react';
+import AsyncSelect from 'react-select/async';
+import { throttle, debounce } from "throttle-debounce";
 import searchIcon from '../../images/search.svg';
 import './SearchBar.css';
 
 function SearchBar({
-  onSearch,
+  getPlacesForDropdown,
+  getSimilarPlaces,
   filters,
+  dropdownPlaces
 }) {
   const [term, setTerm] = useState('');
+  const [placeId, setPlaceId] = useState('')
+
+  const setSimilarPlaceId = (val, label) => {
+    setPlaceId(String(val));
+    setTerm(label);
+  }
+  
+  const getOptions = (inputValue) => {
+    new Promise(resolve => {
+      setTimeout(() => {
+        setTerm(inputValue);
+        getPlacesForDropdown(inputValue);
+        resolve(dropdownPlaces);
+      }, 100)
+    })
+  }
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
-      onSearch(term, filters);
+      getSimilarPlaces(placeId, filters)
     }
-  };
+  }
+  const suggestionHtml = () => {
+    if (term.length > 2) {
+      return (
+        <div id="dropdown-container">
+          <ul id="selectMenu">
+            {dropdownPlaces.map((place, index)=>{
+              return <li key={index} onClick={() => {setSimilarPlaceId(place.value, place.label)}} value={place.value}>{place.label}</li>
+            })}
+          </ul>
+        </div>
+      )
+    }
+  }
   return (
+    <div id="search-bar-container">
     <div id='main-search'>
       <input
         type="text"
         placeholder="Search Climate Tree"
         value={term}
-        onChange={(event) => setTerm(event.target.value)}
+        onChange={(event) => getOptions(event.target.value)}
         onKeyDown={(event) => handleKeyDown(event)}
       />
       <img
         src={searchIcon}
         alt="search"
         id="search"
-        onClick={() => onSearch(term, filters)}
+        onClick={(event) =>{getSimilarPlaces(placeId, filters)}}
       />
-    </div>
+      <br/>
+      </div>
+      {suggestionHtml()}
+      </div>
   );
 }
 
