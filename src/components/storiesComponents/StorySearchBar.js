204850c15073cdc390c201ce46e166762bda2a73
Theodore Andrew <andrew.t@husky.neu.edu>
2020-02-21 01:28:34 -0800

    styling search bar

--- src/components/storiesComponents/StorySearchBar.js
+++ src/components/storiesComponents/StorySearchBar.js
@@ -10,14 +10,20 @@ const StorySearchBar = ({ searchTerm, history }) => {
   };
 
   return (
-    <div>
+    <div className="search-bar-container">
+      <h2 id="search-for-stories-title">Search For Stories</h2>
       <form onSubmit={e => onSubmitForm(e)}>
         <input
           type="text"
+          id="search-for-story"
           value={term}
+          placeholder="Enter Story Title"
           onChange={e => setTerm(e.target.value)}
         />
       </form>
+      <div className="result-term">
+        Results for: <span id="search-term">{searchTerm}</span>
+      </div>
     </div>
   );
 };
