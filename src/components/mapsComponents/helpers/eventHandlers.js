475c57d036d0d2cf0a679a22fc25cd2bdbeb4015
Evan <EvanLDouglass@gmail.com>
2020-02-24 12:15:26 -0800

    Modify popup content to be a react component

--- src/components/mapsComponents/helpers/eventHandlers.js
+++ src/components/mapsComponents/helpers/eventHandlers.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 /**
  * Handles the location and contents of popups on the map.
  * @param {event} evt A single click event
@@ -5,41 +8,65 @@
  * @param {Overlay} overlay An OpenLayers Overlay object
  */
 const popUpHandler = (evt, map, overlay) => {
+  // Get list of features
   let pixel = evt.pixel;
-  // Highly nested data! These are the places in an array
   let features = map.getFeaturesAtPixel(pixel);
-  // These are the coordinates of the point
-  let coord = features[0].getProperties().geometry.flatCoordinates;
+
   // Ignore clicks on map that aren't on a feature
   if (features.length === 0) {
     overlay.setPosition(undefined);
     return;
   }
 
+  // Get places contained in the feature list
   let places = map.getFeaturesAtPixel(pixel)[0].getProperties().features;
+
+  // Set a div for popup content
+  // Due to OL library, this needs to be done on the actual DOM
   let content = document.getElementById("popup-content");
-  let html, place;
-  if (places.length > 1) {
-    // This is a cluster
-    html = `
-            <p>
-                There are ${places.length} places in this area.<br>
-                Zoom in to see details.
-            </p>
-        `;
-  } else {
-    // This is a single place
-    place = places[0].getProperties();
-    html = `
-            <p>
-                Name: <strong>${place.name}</strong><br>
-                Place Type: ${place.typeName}<br>
-                Population: ${place.population}<br>
-                Carbon: ${place.carbon}
-            </p>
-        `;
-  }
-  content.innerHTML = html;
+  content.innerHTML = `<div id="popup-html"></div>`;
+
+  const testFunc = () => {
+    console.log("The func worked");
+  };
+
+  const PopupContent = () => {
+    if (places.length > 1) {
+      // Cluster popup
+      return (
+        <>
+          <p>
+            There are {places.length} places in this area.
+            <br />
+            Zoom in to see details.
+          </p>
+        </>
+      );
+    } else {
+      // Single place popup
+      let place = places[0].getProperties();
+      return (
+        <>
+          <p>
+            Name: <strong>{place.name}</strong>
+            <br />
+            Place Type: {place.typeName}
+            <br />
+            Population: {place.population}
+            <br />
+            Carbon: {place.carbon}
+          </p>
+          <button onClick={testFunc}>Click</button>
+        </>
+      );
+    }
+  };
+
+  // Render the popup content
+  ReactDOM.render(<PopupContent />, document.getElementById("popup-html"));
+
+  // Set the popup location to the coordinates of the point
+  let coord = features[0].getProperties().geometry.flatCoordinates;
   overlay.setPosition(coord);
 };
 
