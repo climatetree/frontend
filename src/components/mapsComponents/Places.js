affcee28bfd4abfc88feb337c36ab742a7eb8593
Evan <EvanLDouglass@gmail.com>
2020-02-27 21:53:18 -0800

    Bug fix: update map points on new search. Remove debuging prints

--- src/components/mapsComponents/Places.js
+++ src/components/mapsComponents/Places.js
@@ -2,21 +2,20 @@ import React from "react";
 import "./Places.css";
 
 export default function Places({ features }) {
-  console.log("PLACES", features);
   return (
     <div className="places-container">
       <p>Total: {!features ? 0 : features.length}</p>
       {!features
         ? ""
         : features.map(({ properties }, index) => (
-            <div key={properties.place_id}>
-              <p>{index + 1}</p>
-              <p>Name: {properties.name}</p>
-              <p>Type Name: {properties.typeName}</p>
-              <p>Population: {properties.population}</p>
-              <p>Carbon: {properties.carbon}</p>
-            </div>
-          ))}
+          <div key={properties.place_id}>
+            <p>{index + 1}</p>
+            <p>Name: {properties.name}</p>
+            <p>Type Name: {properties.typeName}</p>
+            <p>Population: {properties.population}</p>
+            <p>Carbon: {properties.carbon}</p>
+          </div>
+        ))}
     </div>
   );
 }
