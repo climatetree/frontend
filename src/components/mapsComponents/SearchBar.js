64ccce369e1075a21a6178a379426398898e832c
Xiaoyi <sunxiaoyi2014@gmail.com>
2020-03-13 16:26:32 -0700

    search flow enhancement

--- src/components/mapsComponents/SearchBar.js
+++ src/components/mapsComponents/SearchBar.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
 import useDebounce from "./helpers/useDebounce";
 import searchIcon from "../../images/search.svg";
+import closeIcon from '../../images/x.svg';
 import "./SearchBar.css";
 
 export default function SearchBar({
-  getExactPlaces,
   getSimilarPlaces,
   filters,
-  similarPlacesEnabled
+  targetPlaceID,
+  setTargetPlace,
 }) {
   const [searchTerm, setSearchTerm] = useState("");
-  const [placeSuggestions, setPlaceSuggestions] = useState([]);
-  const [isSearching, setIsSearching] = useState(false);
   const debouncedSearchTerm = useDebounce(searchTerm, 1000);
+  const [isSearching, setIsSearching] = useState(false);
+  const [placeSuggestions, setPlaceSuggestions] = useState([]);
+  const [selectedSuggestion, setSelectedSuggestion] = useState([]);
   useEffect(() => {
     if (debouncedSearchTerm) {
       setIsSearching(true);
@@ -23,24 +25,55 @@ export default function SearchBar({
         .then(results => {
           setIsSearching(false);
           setPlaceSuggestions(results.features);
+          setSelectedSuggestion([
+            results.features[0].properties.place_id,
+            0,
+          ]);
         });
     } else {
       setPlaceSuggestions([]);
+      setSelectedSuggestion([]);
     }
   }, [debouncedSearchTerm]);
 
-  // const handleKeyDown = (event) => {
-  //   if (event.key === "Enter") {
-  //     getSimilarPlaces(placeId, filters);
-  //   }
-  // };
-
-  const handleSuggestionClick = (placeId, name) => {
+  const openConfirmationPanel = () => {
+    document.getElementById('suggestions').style.display = 'none';
+    document.getElementById('confirmation').style.display = 'flex';
+  }
+  const closeConfirmationPanel = () => {
+    document.getElementById('confirmation').style.display = 'none';
+  }
+  const handleKeyDown = (event) => {
+    if (isSearching) {
+      return;
+    }
+    if (event.key === 'Enter') {
+      if (placeSuggestions.length > 1) {
+        openConfirmationPanel();
+      } else if (
+        placeSuggestions.length === 1 &&
+        placeSuggestions[0].properties.place_id !== targetPlaceID
+      ) {
+        setSearchTerm(placeSuggestions[0].properties.name);
+        setSelectedSuggestion([
+          placeSuggestions[0].properties.place_id,
+          0,
+        ]);
+        setTargetPlace(placeSuggestions[0]);
+        getSimilarPlaces(
+          placeSuggestions[0].properties.place_id,
+          filters
+        );
+        document.getElementById('suggestions').style.display = 'none';
+      }
+    }
+  };
+  const handleSuggestionClick = (placeID, name, index) => {
     setSearchTerm(name);
-    if (similarPlacesEnabled) {
-      getSimilarPlaces(placeId, filters);
-    } else {
-      getExactPlaces(name, filters);
+    if (placeID !== targetPlaceID) {
+      setSelectedSuggestion([placeID, index]);
+      setTargetPlace(placeSuggestions[index]);
+      getSimilarPlaces(placeID, filters);
     }
   };
 
@@ -52,7 +85,7 @@ export default function SearchBar({
           placeholder="Search by place name"
           value={searchTerm}
           onChange={event => setSearchTerm(event.target.value)}
-          // onKeyDown={event => handleKeyDown(event)}
+          onKeyDown={event => handleKeyDown(event)}
           onFocus={() => {
             document.querySelector("#suggestions").style.display = "block";
           }}
@@ -62,18 +95,33 @@ export default function SearchBar({
             }, 200);
           }}
         />
-
         <img
           src={searchIcon}
           alt="search"
           id="search"
-        // onClick={() => {
-        //   if (similarPlacesEnabled) {
-        //     getSimilarPlaces(placeId);
-        //   } else {
-        //     getExactPlaces(debouncedSearchTerm);
-        //   }
-        // }}
+          onClick={() => {
+            if (isSearching) {
+              return;
+            }
+            if (placeSuggestions.length > 1) {
+              openConfirmationPanel();
+            } else if (
+              placeSuggestions.length === 1 &&
+              placeSuggestions[0].properties.place_id !== targetPlaceID
+            ) {
+              setSearchTerm(placeSuggestions[0].properties.name);
+              setSelectedSuggestion([
+                placeSuggestions[0].properties.place_id,
+                0,
+              ]);
+              setTargetPlace(placeSuggestions[0]);
+              getSimilarPlaces(
+                placeSuggestions[0].properties.place_id,
+                filters
+              );
+              document.getElementById('suggestions').style.display = 'none';
+            }
+          }}
         />
       </div>
       <div id="suggestions">
@@ -81,23 +129,20 @@ export default function SearchBar({
           <p>Searching...</p>
         ) : placeSuggestions.length > 0 ? (
           <>
-            {placeSuggestions.map(({ properties }) => {
+            {placeSuggestions.map(({ properties }, index) => {
               const { place_id, name, state_name, nation_name } = properties;
-
               return (
                 <p
+                  className={`place-name-dropdown${place_id === selectedSuggestion[0] ? ' highlight' : ''}`}
                   key={place_id}
                   onClick={() => {
-                    handleSuggestionClick(place_id, name);
+                    handleSuggestionClick(place_id, name, index);
                   }}
                 >
-                  <span className="place-name-dropdown">{name} </span>
-                  <div className="state-nation-name-dropdown-container">
-                    <span className="state-nation-name-dropdown">
-                      {state_name}
-                      {state_name ? "," : ""} {nation_name}
-                    </span>
-                  </div>
+                  {name}
+                  <span className="state-nation-name-dropdown">
+                    {state_name} {state_name ? "," : ""} {nation_name}
+                  </span>
                 </p>
               );
             })}
@@ -105,8 +150,37 @@ export default function SearchBar({
         ) : debouncedSearchTerm.length > 0 ? (
           <p>No suggestion</p>
         ) : (
-                <p>User Search History</p>
-              )}
+          <p>User Search History</p>
+        )}
+      </div>
+      <div id="confirmation">
+        <div id="confirmation-panel">
+          <img
+            className="close-btn"
+            src={closeIcon}
+            alt="close suggestion"
+            onClick={closeConfirmationPanel}
+          />
+          <p className="confirmation-title">Did you mean</p>
+          {placeSuggestions.map(({ properties }, index) => {
+            const { place_id, name, state_name, nation_name } = properties;
+            return (
+              <div
+                className="confirmation-item"
+                key={place_id}
+                onClick={() => {
+                  handleSuggestionClick(place_id, name, index);
+                  closeConfirmationPanel();
+                }}
+              >
+                <p>{name}</p>
+                <p className="state-nation-name-dropdown">
+                  {state_name} {state_name ? "," : ""} {nation_name}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
