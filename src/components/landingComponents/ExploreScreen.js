1a9ad8b5bc5506f2e204a482e4ca7e082d5087a5
Theodore Andrew <andrew.t@husky.neu.edu>
2020-03-14 17:00:15 -0700

    add initial design for displaying top stories

--- src/components/landingComponents/ExploreScreen.js
+++ src/components/landingComponents/ExploreScreen.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { ReactTinyLink } from "react-tiny-link";
+
 import "../../styles/ExploreScreen.css";
 
 function ExploreScreen() {
+  useEffect(() => {
+    console.log("HELLO FROM EXPLORE!!");
+  });
+
   return (
     <section id="explore-screen">
-      <h2 id="explore-header">Explore climate actions</h2>
+      <h2 id="explore-header">Explore Top Popular Stories</h2>
       <div className="explore-tile">
+        <ReactTinyLink
+          cardSize="large"
+          showGraphic={true}
+          maxLine={2}
+          minLine={1}
+          width={300}
+          url={"https://en.wikipedia.org/wiki/Seattle"}
+        />
+      </div>
+      <div className="explore-tile">
+        <ReactTinyLink
+          cardSize="large"
+          showGraphic={true}
+          maxLine={2}
+          minLine={1}
+          width={300}
+          url={"https://en.wikipedia.org/wiki/Los_Angeles"}
+        />
+      </div>
+      <div className="explore-tile">
+        <ReactTinyLink
+          cardSize="large"
+          showGraphic={true}
+          maxLine={2}
+          minLine={1}
+          width={300}
+          url={"https://en.wikipedia.org/wiki/Japan"}
+        />
+      </div>
+      {/* <div className="explore-tile">
         <img src="https://picsum.photos/240/100" alt="" />
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod eaque
@@ -18,14 +54,7 @@ function ExploreScreen() {
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod eaque
           sunt velit ducimus! Possimus, ex a fugiat voluptatem nostrum aut.
         </p>
-      </div>
-      <div className="explore-tile">
-        <img src="https://picsum.photos/240/100" alt="" />
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod eaque
-          sunt velit ducimus! Possimus, ex a fugiat voluptatem nostrum aut.
-        </p>
-      </div>
+      </div> */}
     </section>
   );
 }
