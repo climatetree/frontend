73500678373b9550d18b6a3b944151dd83e7020d
Theodore Andrew <andrew.t@husky.neu.edu>
2020-03-14 17:58:31 -0700

    styling top stories

--- src/components/landingComponents/ExploreScreen.js
+++ src/components/landingComponents/ExploreScreen.js
@@ -15,7 +15,10 @@ function ExploreScreen() {
         <ReactTinyLink
           cardSize="large"
           showGraphic={true}
-          maxLine={2}
+          description={
+            "Seattle (/siˈætəl/ (About this soundlisten) see-AT-əl) is a seaport city on the West Coast of the United States. It is the seat of King County, Washington. With an estimated 744,955 residents as of 2018, Seattle is the largest city in both the state of Washington and the Pacific Northwest region of North America."
+          }
+          maxLine={3}
           minLine={1}
           width={300}
           url={"https://en.wikipedia.org/wiki/Seattle"}
@@ -25,7 +28,10 @@ function ExploreScreen() {
         <ReactTinyLink
           cardSize="large"
           showGraphic={true}
-          maxLine={2}
+          description={
+            "Los Angeles (/lɔːs ˈændʒələs/ (About this soundlisten);[a] Spanish: Los Ángeles; Spanish for '\"The Angels\"'),[16] officially the City of Los Angeles and often known by its initials L.A., is the most populous city in California; the second most populous city in the United States, after New York City; and the third most populous city in North America, after Mexico City and New York City."
+          }
+          maxLine={3}
           minLine={1}
           width={300}
           url={"https://en.wikipedia.org/wiki/Los_Angeles"}
@@ -35,7 +41,10 @@ function ExploreScreen() {
         <ReactTinyLink
           cardSize="large"
           showGraphic={true}
-          maxLine={2}
+          description={
+            "Japan (Japanese: 日本, Nippon [ɲippoꜜɴ] (About this soundlisten) or Nihon [ɲihoꜜɴ] (About this soundlisten); officially 日本国, About this soundNippon-koku or Nihon-koku) is an island country located in East Asia. It is bordered by the Sea of Japan to the west and the Pacific Ocean to the east, and spans from the Sea of Okhotsk in the north to the East China Sea and Philippine Sea in the south."
+          }
+          maxLine={3}
           minLine={1}
           width={300}
           url={"https://en.wikipedia.org/wiki/Japan"}
