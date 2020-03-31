ecd04c5cbe68ed3a7b05e7cb1c10d72349d41cfe
Xiaoyi <sunxiaoyi2014@gmail.com>
2020-03-30 23:59:14 -0700

    Add place suggestions to post story form

--- src/components/loginComponents/PostStoryForm.js
+++ src/components/loginComponents/PostStoryForm.js
@@ -1,64 +1,176 @@
-import React from 'react';
-import { Formik } from 'formik';
-import closeIcon from '../../images/x.svg';
+import React, { useContext, useState, useEffect } from 'react';
+import { UserContext } from "../context/UserContext";
+import useDebounce from '../mapsComponents/helpers/useDebounce';
+import CloseIcon from '../../images/x.svg';
 import './PostStoryForm.css';
 
 export default function PostStoryForm({
   setOpenPostStoryForm,
+  myStories,
+  setMyStories,
 }) {
+  const { user } = useContext(UserContext);
   const closeForm = () => {
     setOpenPostStoryForm(false)
   }
+  function handleSubmit(hyperlink, story_title, place_id) {
+    console.log({
+      user_id: user.userId,
+      hyperlink,
+      story_title,
+      place_ids: [place_id],
+      date: Date().toString(),
+    });
+    // fetch('localhost:3000/stories/create', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     user_id: user.userId,
+    //     hyperlink,
+    //     story_title,
+    //     place_ids: [place_id],
+    //     date: Date().toString(),
+    //   }),
+    // }).then(() => {
+      setMyStories([
+        ...myStories,
+        {
+          user_id: user.userId,
+          hyperlink,
+          story_title,
+          date: Date().toString(),
+        }
+      ]);
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
+    closeForm();
+  }
+  const [hyperlink, setHyperlink] = useState('');
+  const [place, setPlace] = useState('');
+  const debouncedSearchTerm = useDebounce(place, 1000);
+  const [isSearchingSuggestions, setIsSearchingSuggestions] = useState(false);
+  const [placeSuggestions, setPlaceSuggestions] = useState([]);
+  const [selectedPlaceID, setSelectedPlaceID] = useState(null);
+  useEffect(() => {
+    if (debouncedSearchTerm && debouncedSearchTerm.length > 1) {
+      setIsSearchingSuggestions(true);
+      fetch(
+        `https://climatetree-api-gateway.azurewebsites.net/places/${debouncedSearchTerm}`
+      )
+        .then(response => response.json())
+        .then(results => {
+          if (results.features) {
+            setPlaceSuggestions(results.features);
+            setSelectedPlaceID(results.features[0].properties.place_id);
+          } else {
+            setPlaceSuggestions([]);
+            setSelectedPlaceID(null);
+          }
+          setIsSearchingSuggestions(false);
+        });
+    } else {
+      setPlaceSuggestions([]);
+      setSelectedPlaceID(null);
+    }
+    return () => {
+      
+    }
+  }, [debouncedSearchTerm]);
   return (
     <section className="story-form-wrapper">
-      <Formik
-        initialValues={{
-          storyTitle: '',
-          storyLink: '',
-        }}
-        onSubmit={(values) => {
-          console.log('submitting', values);
-          closeForm();
-        }}
-      >
-        {({ handleSubmit, handleChange, values }) => (
-          <form onSubmit={handleSubmit}>
-            <header>
-              <p className="">Post New Story</p>
-              <div className="close-btn">
-                <img
-                  src={closeIcon}
-                  alt="close post story form"
-                  onClick={closeForm}
-                />
-              </div>
-            </header>
-            <div className="form-body">
-              <label htmlFor="storyTitle">Title</label>
-              <input
-                id="storyTitle"
-                name="storyTitle"
-                type="text"
-                onChange={handleChange}
-                value={values.storyTitle}
-                placeholder="Story Title"
-              />
-              <label htmlFor="storyLink">Link</label>
-              <input
-                id="storyLink"
-                name="storyLink"
-                type="text"
-                onChange={handleChange}
-                value={values.storyLink}
-                placeholder="Story Hyperlink"
-              />
-            </div>
-            <footer>
-              <button className="post-story" type="submit">Post</button>
-            </footer>
-          </form>
-        )}
-      </Formik>
+      <form autoComplete="off">
+        <header>
+          <p className="">Post New Story</p>
+          <div className="close-btn">
+            <img
+              src={CloseIcon}
+              alt="close post story form"
+              onClick={closeForm}
+            />
+          </div>
+        </header>
+        <div className="form-body">
+          <label htmlFor="hyperlink">Link</label>
+          <input
+            id="hyperlink"
+            name="hyperlink"
+            type="text"
+            value={hyperlink}
+            onChange={(event) => {
+              setHyperlink(event.target.value);
+            }}
+            placeholder="Story Hyperlink"
+          />
+          <label htmlFor="place">Place</label>
+          <input
+            id="place"
+            name="place"
+            type="text"
+            placeholder="Story Place"
+            value={place}
+            onChange={(event) => setPlace(event.target.value)}
+            onFocus={() => {
+              document.querySelector("#place-suggestions").style.display = "block";
+            }}
+            onBlur={() => {
+              setTimeout(() => {
+                const suggestions = document.querySelector("#place-suggestions");
+                if (suggestions) {
+                  suggestions.style.display = "none";
+                }
+              }, 200);
+            }}
+          />
+        </div>
+        <div id="place-suggestions">
+          {isSearchingSuggestions ? (
+            <p className="hint">Searching...</p>
+          ) : placeSuggestions.length > 0 ? (
+            <>
+              {placeSuggestions.map(({ properties }) => {
+                const { place_id, name, state_name, nation_name } = properties;
+                return (
+                  <p
+                    className={`place-name-dropdown${place_id === selectedPlaceID[0] ? ' highlight' : ''}`}
+                    key={place_id}
+                    onClick={() => {
+                      setPlace(name);
+                      setSelectedPlaceID(place_id);
+                    }}
+                  >
+                    {name}
+                    <span className="state-nation-name-dropdown">
+                      {state_name} {state_name ? ',' : ''} {nation_name}
+                    </span>
+                  </p>
+                );
+              })}
+            </>
+          ) : debouncedSearchTerm.length === 1 ? (
+            <p className="hint">Please enter more than 1 letter</p>
+          ) : debouncedSearchTerm.length > 0 ? (
+            <p className="hint">No suggestion</p>
+          ) : (
+            <p className="hint">Place Suggestions</p>
+          )}
+        </div>
+        <footer>
+          <button
+            className="post-story"
+            type="button"
+            onClick={async () => {
+              if (selectedPlaceID) {
+                const response = await fetch(`http://localhost:3000/stories/getPreview?hyperlink=${encodeURIComponent(hyperlink)}`);
+                const preview = await response.json();
+                handleSubmit(hyperlink, preview.title, selectedPlaceID);
+              }
+            }}
+          >Post</button>
+        </footer>
+      </form>
     </section>
   );
 }
--- src/components/loginComponents/Profile.js
+++ src/components/loginComponents/Profile.js
@@ -1,18 +1,10 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import { ReactTinyLink } from "react-tiny-link";
 import PostStoryForm from './PostStoryForm';
 import plusIcon from '../../images/plus.svg';
 import { UserContext } from "../context/UserContext";
 import './Profile.css';
 
-const pendingStories = [{
-  description: "Seattle (/siˈætəl/ (About this soundlisten) see-AT-əl) is a seaport city on the West Coast of the United States. It is the seat of King County, Washington. With an estimated 744,955 residents as of 2018, Seattle is the largest city in both the state of Washington and the Pacific Northwest region of North America.",
-  url: "https://en.wikipedia.org/wiki/Seattle",
-}, {
-  description: "Los Angeles (/lɔːs ˈændʒələs/ (About this soundlisten);[a] Spanish: Los Ángeles; Spanish for '\"The Angels\"'),[16] officially the City of Los Angeles and often known by its initials L.A., is the most populous city in California; the second most populous city in the United States, after New York City; and the third most populous city in North America, after Mexico City and New York City.",
-  url: "https://en.wikipedia.org/wiki/Los_Angeles",
-}];
-const myStories = [];
 const trendingStories = [{
   description: "Seattle (/siˈætəl/ (About this soundlisten) see-AT-əl) is a seaport city on the West Coast of the United States. It is the seat of King County, Washington. With an estimated 744,955 residents as of 2018, Seattle is the largest city in both the state of Washington and the Pacific Northwest region of North America.",
   url: "https://en.wikipedia.org/wiki/Seattle",
@@ -27,8 +19,7 @@ const trendingStories = [{
 export default function Profile() {
   const { user } = useContext(UserContext);
   const [openPostStoryForm, setOpenPostStoryForm] = useState(false);
-  useEffect(() => {
-  }, []);
+  const [myStories, setMyStories] = useState([]);
   return (
     <div className="profile-wrapper">
       <div className="profile">
@@ -38,59 +29,26 @@ export default function Profile() {
         </div>
       </div>
       <div className="stories-wrapper">
-        <h2>Pending</h2>
-        <div className="pending stories">
-          {pendingStories.length ? (
-            <>
-              {pendingStories.map(({description, url}, index) => (
-                <div
-                  key={index}
-                  className="pending-wrapper"
-                >
-                  <ReactTinyLink
-                    cardSize="small"
-                    showGraphic={true}
-                    description={description}
-                    maxLine={3}
-                    minLine={3}
-                    url={url}
-                  />
-                  <div className="story-btn-wrapper">
-                    <button className="story-btn">Approve</button>
-                    <button className="story-btn">Delete</button>
-                  </div>
-                </div>
-              ))}
-            </>
-          ) : (
-            <p style={{ textTransform: "capitalize" }}>No pending stories</p>
-          )}
-        </div>
         <h2>My stories</h2>
         <div className="personal stories">
-          {myStories.length ? (
-            <>
-              {myStories.map(({description, url}, index) => (
-                <ReactTinyLink
-                  key={index}
-                  cardSize="small"
-                  showGraphic={true}
-                  description={description}
-                  maxLine={3}
-                  minLine={3}
-                  url={url}
-                />
-              ))}
-            </>
-          ) : (
-            <div
-              className="post-story"
-              onClick={() => setOpenPostStoryForm(true)}
-            >
-              <img src={plusIcon} alt="post story icon"/>
-              <p>post a story</p>
-            </div>
-          )}
+          {myStories.map(({ hyperlink }, index) => (
+            <ReactTinyLink
+              key={index}
+              cardSize="small"
+              showGraphic={true}
+              // description={description}
+              maxLine={3}
+              minLine={3}
+              url={hyperlink}
+            />
+          ))}
+          <div
+            className="post-story"
+            onClick={() => setOpenPostStoryForm(true)}
+          >
+            <img src={plusIcon} alt="post story icon"/>
+            <p>post a story</p>
+          </div>
         </div>
         <h2>Trending</h2>
         <div className="trending stories">
@@ -110,6 +68,8 @@ export default function Profile() {
       {openPostStoryForm &&
         <PostStoryForm
           setOpenPostStoryForm={setOpenPostStoryForm}
+          myStories={myStories}
+          setMyStories={setMyStories}
         />
       }
     </div>
