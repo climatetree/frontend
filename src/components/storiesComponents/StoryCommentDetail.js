a90d4606af8fd5d925674bd641ad5c54c7f61208
Theodore Andrew <andrew.t@husky.neu.edu>
2020-03-31 16:53:12 -0700

    fix "time since" to be dynamic

--- src/components/storiesComponents/StoryCommentDetail.js
+++ src/components/storiesComponents/StoryCommentDetail.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 import { UserContext } from "../context/UserContext";
 
@@ -8,8 +8,17 @@ const StoryCommentDetail = ({
   commentId,
   onChangeDeleteComment
 }) => {
+  useEffect(() => {
+    (() => {
+      onChangeTimeSince();
+    })();
+  }, []);
+
   const { user } = useContext(UserContext);
   const { role, userId, jwt } = user;
+
+  const [timeSince, setTimeSince] = useState(0);
+  const [timeSinceAlphabet, setTimeSinceAlphabet] = useState("s");
 
   const deleteComment = async () => {
     const comment = {
@@ -36,6 +45,57 @@ const StoryCommentDetail = ({
     }
   };
 
+  const doesConvertToYear = seconds => {
+    return seconds > 31536000;
+  };
+
+  const doesConvertToMonth = seconds => {
+    return seconds > 2592000;
+  };
+
+  const doesConvertToDay = seconds => {
+    return seconds > 86400;
+  };
+
+  const doesConvertToHour = seconds => {
+    return seconds > 3600;
+  };
+
+  const doesConvertToMinutes = seconds => {
+    return seconds > 60;
+  };
+
+  const doesConvertToSeconds = seconds => {
+    return seconds;
+  };
+
+  const onChangeTimeSince = () => {
+    const seconds = Math.floor((Date.now() - new Date(comment.date)) / 1000);
+    // const interval = Math.floor(seconds / 31536000);
+
+    if (doesConvertToYear(seconds)) {
+      setTimeSince(Math.floor(seconds / 31536000));
+      setTimeSinceAlphabet("y");
+    } else if (doesConvertToMonth(seconds)) {
+      setTimeSince(Math.floor(seconds / 2592000));
+      setTimeSinceAlphabet("m");
+    } else if (doesConvertToDay(seconds)) {
+      setTimeSince(Math.floor(seconds / 86400));
+      setTimeSinceAlphabet("d");
+    } else if (doesConvertToHour(seconds)) {
+      setTimeSince(Math.floor(seconds / 3600));
+      setTimeSinceAlphabet("h");
+    } else if (doesConvertToMinutes(seconds)) {
+      setTimeSince(Math.floor(seconds / 60));
+      setTimeSinceAlphabet("m");
+    } else {
+      setTimeSince(seconds);
+      setTimeSinceAlphabet("s");
+    }
+  };
+
+  console.log(timeSince + timeSinceAlphabet);
+
   return (
     <div className="comment-detail">
       <div>
@@ -45,7 +105,9 @@ const StoryCommentDetail = ({
         <span className="comment-content">{comment.content}</span>
       </div>
       <div className="comment-footer">
-        <span className="comment-hours-ago">16h</span>
+        <span className="comment-hours-ago">
+          {timeSince + timeSinceAlphabet}
+        </span>
         <span className="comment-likes-count">6 likes</span>
         {userId === comment.user_id && (
           <button onClick={deleteComment} className="delete-comment-btn">
