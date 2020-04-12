5f5ed10f82a0b12748497c5a878df9fd2de00c28
Evan <EvanLDouglass@gmail.com>
2020-04-11 18:42:07 -0700

    Admin users can change other user's roles

--- src/components/loginComponents/ChangeUserRoleForm.js
+++ src/components/loginComponents/ChangeUserRoleForm.js
@@ -10,6 +10,9 @@ const ChangeUserRoleForm = ({
     const { user } = useContext(UserContext);
     const [emailInput, setEmailInput] = useState("");
     const [roleInput, setRoleInput] = useState("");
+    const [submitMsg, setSubmitMsg] = useState("");
+    const [msgIsError, setMsgIsError] = useState(true);
+
 
     // Role ids for user service = index + 1
     const roles = ["administrator", "moderator", "standard user"];
@@ -18,38 +21,84 @@ const ChangeUserRoleForm = ({
         setOpenChangeUserRoleForm(false);
     };
 
+    /**
+     * Update user role if form is valid.
+     */
+    async function handleSubmit() {
+        // Remove previous error messages
+        setSubmitMsg("");
+
+        // Ensure valid role chosen
+        let roleId = roles.indexOf(roleInput) + 1;  // 0 if not found
+        if (roleId === 0) {
+            setMsgIsError(true);
+            setSubmitMsg("Invalid role. Choose from the options given.");
+            return;
+        }
+
+        // Ensure user is found
+        let userId = await searchUserEmail(emailInput);
+        if (userId === -1) {
+            setMsgIsError(true)
+            setSubmitMsg("User email not found. Please try again.");
+            return;
+        }
+
+        // Proceed with change role
+        let ok = await updateRole(userId, roleId);
+        if (!ok) {
+            setMsgIsError(true);
+            setSubmitMsg("Unable to update role. Note: user may already have this role.");
+        } else {
+            // Notify user of successful submit
+            setMsgIsError(false);
+            setSubmitMsg("Update successful!");
+        }
+    }
+
+    /**
+     * Retrieves the user id of attached to the user email given.
+     * Returns the user id, or -1 if user is not found.
+     * @param {String} email A user's email to search for
+     */
     async function searchUserEmail(email) {
-        console.log(JSON.stringify({
-            "email": email,
-        }));
-        fetch("https://climatetree-api-gateway.azurewebsites.net/user/searchemail", {
+        return fetch(`https://climatetree-api-gateway.azurewebsites.net/user/searchemail/${email}`, {
             method: "GET",
             headers: {
                 "Authorization": "Bearer " + user.jwt,
                 "Content-Type": "application/json",
             },
-            // convert the data to json
-            body: JSON.stringify({
-                "email": email,
-            }),
         }).then(response => {
-            console.log(response);
+            return response.json();
+        }).then(response => {
+            if (response.users) {
+                return response.users.userId;
+            } else {
+                return -1;
+            }
         }).catch((error) => {
             console.log(error);
+            return -1;
         });
     }
 
+    /**
+     * Changes the user role of the given user.
+     * @param {Integer} userId The ID of the user needing an update
+     * @param {Integer} roleId The ID of the new role for this user
+     */
     async function updateRole(userId, roleId) {
-        fetch(`https://climatetree-api-gateway.azurewebsites.net/user/${userId}/${roleId}`, {
+        return fetch(`https://climatetree-api-gateway.azurewebsites.net/user/${userId}/${roleId}`, {
             method: "PUT",
             headers: {
                 Authorization: "Bearer " + user.jwt,
                 "Content-Type": "application/json",
             },
         }).then(response => {
-            console.log(response);
+            return response.ok;
         }).catch(error => {
             console.log(error);
+            return false;
         });
     }
 
@@ -93,10 +142,15 @@ const ChangeUserRoleForm = ({
                         <button
                             className="post-change"
                             type="button"
-                            onClick={async () => searchUserEmail(emailInput)}
+                            onClick={handleSubmit}
                         >
                             Update
                         </button>
+                        {submitMsg && (
+                            <p className={msgIsError ? "error-msg" : "success-msg"}>
+                                {submitMsg}
+                            </p>
+                        )}
                     </footer>
                 </div>
             </form>
