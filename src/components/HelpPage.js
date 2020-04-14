cab321abfe65ffe7acbbb351094f134ba005b631
Theodore Andrew <andrew.t@husky.neu.edu>
2020-04-14 11:22:15 -0700

    first commit for help page branch

---
+++ src/components/HelpPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelpPage = () => {
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>Help Page will go here</h1>
+    </div>
+  );
+};
+
+export default HelpPage;
